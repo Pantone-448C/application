525f51dedbd47cf108d5370f6edd279d064217f6
tomdx <thomasle0@outlook.com>
2021-09-09 13:55:29 +1000

    Add hello message.

--- lib/home/cubit/trip_cubit.dart
+++ lib/home/cubit/trip_cubit.dart
@@ -17,7 +17,7 @@ class TripCubit extends Cubit<TripState> {
     // this is just a fake async call for testing.
     emit(TripLoading());
     await Future.delayed(Duration(seconds: 2));
-    emit(TripLoaded("Brisbane", 3, 66, 1129, wanderlists));
+    emit(TripLoaded("Brisbane", 3, 66, 1129, wanderlists, "Joe"));
   }
 }
 
--- lib/home/cubit/trip_state.dart
+++ lib/home/cubit/trip_state.dart
@@ -14,13 +14,14 @@ class TripLoading implements TripState {
 
 class TripLoaded extends Equatable implements TripState {
   const TripLoaded(this.name, this.numWanderlists, this.percentageComplete,
-      this.points, this.wanderlists);
+      this.points, this.wanderlists, this.firstName);
 
   final String name;
   final int numWanderlists;
   final int percentageComplete;
   final int points;
   final List<UserWanderlist> wanderlists;
+  final String firstName;
 
   @override
   List<Object?> get props =>
@@ -31,12 +32,15 @@ class TripLoaded extends Equatable implements TripState {
       int? numWanderlists,
       int? percentageComplete,
       int? points,
-      List<UserWanderlist>? wanderlists}) {
+      List<UserWanderlist>? wanderlists,
+      String? firstName}) {
     return TripLoaded(
         name ?? this.name,
         numWanderlists ?? this.numWanderlists,
         percentageComplete ?? this.percentageComplete,
         points ?? this.points,
-        wanderlists ?? this.wanderlists);
+        wanderlists ?? this.wanderlists,
+        firstName ?? this.firstName,
+    );
   }
 }
