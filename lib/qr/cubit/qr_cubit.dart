7c6fcaa82694f10ba669f85b31b5f4dd39efead5
alistair <alistair@topost.net>
2021-09-13 16:13:47 +1000

    Read qr

---
+++ lib/qr/cubit/qr_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+import 'qr_state.dart';
+
+class QrCubit extends Cubit<QrScannerState> {
+  QrCubit() : super(QrScannerFinished());
+
+  void gotCode(String c) {
+    emit(AddActivity(c));
+  }
+
+}
+
+
+
