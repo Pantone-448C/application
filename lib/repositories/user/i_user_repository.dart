db77ad0da561e2c3779e6083e80c2906646ac928
tomdx <thomasle0@outlook.com>
2021-09-19 19:36:43 +1000

    Add fine-grained userwanderlist updater and completed activities updater

--- lib/repositories/user/i_user_repository.dart
+++ lib/repositories/user/i_user_repository.dart
@@ -4,11 +4,12 @@ import 'package:application/models/user_wanderlist.dart';
 
 abstract class IUserRepository {
   Future<UserDetails> getUserData();
-  Future<UserDetails> getUserDataAndWanderlists();
   Future<Iterable<UserWanderlist>> getActiveWanderlists();
   Future<Iterable<UserWanderlist>> getUserWanderlists();
   Future<Iterable<ActivityDetails>> getUserCompletedActivities();
   Future<void> updateUserData(UserDetails details);
+  Future<void> updateUserWanderlists(List<UserWanderlist> list);
+  Future<void> updateUserCompletedActivities(List<ActivityDetails> list);
 
   Future<ActivityDetails> getActivity(String id);
 }
