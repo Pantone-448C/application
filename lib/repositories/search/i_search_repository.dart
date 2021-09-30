289055e644117b11c3443a38aefea57d651abad9
alistair <alistair@topost.net>
2021-09-30 18:21:17 +1000

    WAN-82 implement search repository

---
+++ lib/repositories/search/i_search_repository.dart
@@ -0,0 +1,9 @@
+import 'package:application/models/activity.dart';
+
+/// IActivityRepository provides an interface for access to Activities in
+/// Firestore or a local mock. Since the user can only access repository data
+/// (and not edit it) there is only a single get method.
+abstract class ISearchRepository {
+  Future<List<ActivityDetails>> getNear(double lat, double lon, {double range=0});
+  Future<List<ActivityDetails>> getQuery(String query);
+}
---
+++ lib/repositories/search/search_repository.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:application/activity/view/activity_info.dart';
+import 'package:application/models/activity.dart';
+import 'i_search_repository.dart';
+import 'package:http/http.dart' as http;
+
+
+class SearchRepository implements ISearchRepository {
+  static const API_HOST = "192.168.0.36";
+  static const API_BASE_PATH = "/activity";
+  static const API_PORT = 8080;
+  static const API_SCHEME = 'http';
+
+  _responseToActivityList(String json) {
+    // strongly assuming well-formed json
+    var results = jsonDecode(json)["results"];
+    results.map((json) => ActivityDetails.fromJson(json));
+    List<ActivityDetails> l = List.empty(growable: true);
+    results.forEach((element) => element["doc_id"] = element["id"]);
+    results.forEach((element) => {l.add(ActivityDetails.fromJson(element))});
+    return l;
+  }
+
+  @override
+  Future<List<ActivityDetails>> getNear(double lat, double lon, {double range=50}) async {
+    final response = await http.get(Uri(
+      scheme: API_SCHEME,
+      host: API_HOST,
+      path: API_BASE_PATH + "/near",
+      port: API_PORT,
+      queryParameters: {"lat": lat.toString(), "lon": lon.toString(), "range": range.toString()},
+    ));
+
+    if (response.statusCode == 200) {
+      return _responseToActivityList(response.body);
+    }
+
+    throw Exception(["Bad request"]);
+  }
+
+  @override
+  Future<List<ActivityDetails>> getQuery(String query) async {
+    final response = await http.get(Uri(
+      scheme: API_SCHEME,
+      host: API_HOST,
+      path: API_BASE_PATH + "/search",
+      port: API_PORT,
+      queryParameters: {"query": query},
+    ));
+
+    if (response.statusCode == 200) {
+      return _responseToActivityList(response.body);
+    }
+
+    throw Exception(["Bad request"]);
+  }
+
+}
