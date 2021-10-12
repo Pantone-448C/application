a2224658e2709c15d0c9c11a12d67593f84298d4
Richal Verma <richalverma00@gmail.com>
2021-10-12 23:17:14 +1000

    WAN-120 redesign profile page (#44)
    * Update user info design to match new figma design
    * Add logout and settings button as well as Profile heading
    * Add better loading indicators and text for rewards section

--- lib/profile/view/profile_page.dart
+++ lib/profile/view/profile_page.dart
@@ -11,58 +11,163 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class _UserPhoto extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ProfileCubit, ProfileState>(
-        buildWhen: (prev, next) => true,
-        builder: (context, state) {
-          if (state is ProfileInitial) {
-            return CircularProgressIndicator();
-          } else if (state is ProfileLoaded) {
-            return Container(
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: NetworkImage(state.imgUrl),
-                ),
+      buildWhen: (prev, next) => true,
+      builder: (context, state) {
+        if (state is ProfileLoaded) {
+          return Container(
+            height: 180,
+            width: 144,
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              image: DecorationImage(
+                fit: BoxFit.fitHeight,
+                image: NetworkImage(state.imgUrl),
               ),
-            );
-          } else {
-            return Container(child: Text("Error"));
-          }
-        });
+            ),
+          );
+        } else {
+          return Container();
+        }
+      },
+    );
   }
 }
 
 class _UserInfo extends StatelessWidget {
+  _UserInfo(this.firstName, this.lastName, this.points);
+
+  final String firstName;
+  final String lastName;
+  final int points;
+
+  Widget _name(String firstName, String lastName) {
+    return Text(
+      "$firstName $lastName",
+      style: TextStyle(
+        fontFamily: "inter",
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+    );
+  }
+
+  RichText _bigSmallText(BuildContext context, String big, String small) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: big,
+            style:
+                Theme.of(context).textTheme.headline3?.copyWith(fontSize: 18),
+          ),
+          TextSpan(
+            text: " " + small,
+            style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300,
+                  color: Color(0xFF666666),
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _activityHistoryButton(BuildContext context) {
+    return Container(
+      height: 36,
+      width: 175,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all<EdgeInsets>(
+            EdgeInsets.only(left: 10, right: 0),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(
+            WanTheme.colors.purple,
+          ),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(WanTheme.BUTTON_CORNER_RADIUS),
+              ),
+            ),
+          ),
+        ),
+        onPressed: () => {}, // TODO: make this go to the activity history page
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Activity History",
+              style: TextStyle(
+                fontFamily: "inter",
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Icon(Icons.chevron_right, color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _name(firstName, lastName),
+        Padding(padding: EdgeInsets.only(top: 0.0)),
+        _bigSmallText(context, "$points", "points"),
+        _bigSmallText(context, "12", "completed activities"),
+        Padding(padding: EdgeInsets.only(top: 18.0)),
+        _activityHistoryButton(context),
+        Padding(padding: EdgeInsets.only(top: 8.0)),
+        _LogoutButton(),
+      ],
+    );
+  }
+}
+
+class _UserInfoContainer extends StatelessWidget {
+  static final cardRadius = Radius.circular(WanTheme.CARD_CORNER_RADIUS);
+
+  Container _buildInfoContainerWithChild(BuildContext context, Widget child) {
+    return Container(
+      height: 205,
+      decoration: BoxDecoration(
+        color: Theme.of(context).cardColor,
+        borderRadius: BorderRadius.only(
+          bottomLeft: cardRadius,
+          bottomRight: cardRadius,
+        ),
+      ),
+      padding: EdgeInsets.all(8.0),
+      alignment: Alignment.center,
+      child: child,
+    );
+  }
+
   Widget build(BuildContext context) {
     return BlocBuilder<ProfileCubit, ProfileState>(
       buildWhen: (prev, next) => true,
       builder: (context, state) {
         if (state is ProfileInitial) {
-          return CircularProgressIndicator();
+          return _buildInfoContainerWithChild(
+              context, CircularProgressIndicator());
         } else if (state is ProfileLoaded) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "${state.firstName} ${state.lastName}",
-                style: TextStyle(
-                  fontFamily: "inter",
-                  fontSize: 24,
-                  color: Colors.black,
-                ),
-              ),
-              Text(
-                "Points: ${state.points}",
-                style: TextStyle(
-                  fontFamily: "inter",
-                  fontSize: 24,
-                  color: Colors.black,
-                ),
-              ),
-            ],
+          return _buildInfoContainerWithChild(
+            context,
+            Row(
+              children: [
+                _UserPhoto(),
+                Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 0)),
+                _UserInfo(state.firstName, state.lastName, state.points),
+              ],
+            ),
           );
         } else {
           return Container(child: Text("Error!"));
@@ -72,65 +177,123 @@ class _UserInfo extends StatelessWidget {
   }
 }
 
-class _UserInfoContainer extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Container(
-        height: 200,
-        padding: EdgeInsets.all(10.0),
-        child: Row(
-          children: [
-            _UserPhoto(),
-            Padding(
-              padding: EdgeInsets.fromLTRB(50, 0, 0, 0),
-              child: _UserInfo(),
-            ),
-          ],
-        ));
-  }
-}
-
 class _LogoutButton extends StatelessWidget {
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        FirebaseAuth.instance.signOut();
-        Navigator.popUntil(context, ModalRoute.withName('/'));
-      },
-      child: Text(
-        "Log out",
-        style: TextStyle(
-          color: Colors.white,
-          fontFamily: "inter",
+    return Container(
+      height: 36,
+      width: 175,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all<EdgeInsets>(
+            EdgeInsets.only(left: 10, right: 0),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(
+            WanTheme.colors.pink,
+          ),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(WanTheme.BUTTON_CORNER_RADIUS),
+              ),
+            ),
+          ),
+        ),
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+          Navigator.popUntil(context, ModalRoute.withName('/'));
+        },
+        child: Row(
+          children: [
+            Icon(
+              Icons.logout,
+            ),
+            Text(
+              "Log out",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: "inter",
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
+// TODO: Move this to its own file
 class _RewardsSummary extends StatelessWidget {
+  RichText _title(BuildContext context) {
+    return RichText(
+      text: TextSpan(children: [
+        WidgetSpan(
+          child: Icon(
+            Icons.emoji_events_outlined,
+            color: WanTheme.colors.orange,
+          ),
+        ),
+        TextSpan(
+          text: "Rewards",
+          style: Theme.of(context).textTheme.headline3,
+        ),
+      ]),
+    );
+  }
+
+  RichText _noRewardsText(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+          text: "You have no rewards yet",
+          style: Theme.of(context).textTheme.bodyText1),
+    );
+  }
+
   Widget build(BuildContext context) {
-    return Column(children: [
-      Text("Your rewards"),
-    ]);
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _title(context),
+          Padding(padding: EdgeInsets.only(top: 30)),
+          Center(child: _noRewardsText(context)),
+        ],
+      ),
+    );
   }
 }
 
 class ProfilePage extends StatelessWidget {
+  Widget _settingsButton() {
+    return IconButton(
+      onPressed: () {},
+      icon: Icon(Icons.settings, color: Colors.black54, size: 30),
+    );
+  }
+
+  AppBar _profileAppBar(BuildContext context) {
+    return AppBar(
+      title: Text("Profile", style: Theme.of(context).textTheme.headline2),
+      actions: [_settingsButton()],
+      leading: new IconButton(
+        icon: new Icon(Icons.arrow_back, color: Colors.black),
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => ProfileCubit(UserRepository()),
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-        ),
+        appBar: _profileAppBar(context),
         body: BlocListener<ProfileCubit, ProfileState>(
           listener: (context, state) {},
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               _UserInfoContainer(),
               _RewardsSummary(),
-              _LogoutButton(),
             ],
           ),
         ),
