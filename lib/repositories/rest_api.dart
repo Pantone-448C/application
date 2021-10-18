2cdeed3ae08fb828eb8c32c3faf6055e77d10fc3
Richal Verma <richalverma00@gmail.com>
2021-10-18 18:31:55 +1000

    Fix server ip address

--- lib/repositories/rest_api.dart
+++ lib/repositories/rest_api.dart
@@ -5,7 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 
 const API_SCHEME = 'http';
-const API_HOST = "192.168.0.36";
+const API_HOST = "35.206.254.189";
 const API_PORT = 8080;
 
 getToken() async {
