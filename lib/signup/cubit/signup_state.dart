f7dc45342cc6a765b5a8a383cda44ad69e12dc14
Thomas Le <55824662+tomdx@users.noreply.github.com>
2021-10-18 19:25:30 +1000

    WAN-127 signin signup error handling (#51)

--- lib/signup/cubit/signup_state.dart
+++ lib/signup/cubit/signup_state.dart
@@ -1,40 +1,40 @@
 part of 'signup_cubit.dart';
 
 class SignupState extends Equatable {
-  const SignupState(
-      {this.email = "",
-      this.firstName = "",
-      this.lastName = "",
-      this.password = "",
-      this.passwordConfirm = "",
-      this.isKeyboardOpen = false});
+  const SignupState({
+    this.email = "",
+    this.firstName = "",
+    this.lastName = "",
+    this.password = "",
+    this.passwordConfirm = "",
+    this.signupError = "",
+  });
 
   final String email;
   final String firstName;
   final String lastName;
   final String password;
   final String passwordConfirm;
-  final bool isKeyboardOpen;
+  final String signupError;
 
   @override
   List<Object> get props =>
-      [email, firstName, lastName, password, passwordConfirm, isKeyboardOpen];
+      [email, firstName, lastName, password, passwordConfirm, signupError];
 
-  SignupState copyWith({
-    String? email,
-    String? firstName,
-    String? lastName,
-    String? password,
-    String? passwordConfirm,
-    bool? isKeyboardOpen,
-  }) {
+  SignupState copyWith(
+      {String? email,
+      String? firstName,
+      String? lastName,
+      String? password,
+      String? passwordConfirm,
+      String? signupError}) {
     return SignupState(
       email: email ?? this.email,
       firstName: firstName ?? this.firstName,
       lastName: lastName ?? this.lastName,
       password: password ?? this.password,
       passwordConfirm: passwordConfirm ?? this.passwordConfirm,
-      isKeyboardOpen: isKeyboardOpen ?? this.isKeyboardOpen,
+      signupError: signupError ?? this.signupError,
     );
   }
 }
