7e9e1bd650a8be6db53e0c463854912c03a4f5b0
alistair <alistair@topost.net>
2021-10-21 03:14:12 +1000

    refine account page

--- lib/rewards/view/reward_list.dart
+++ lib/rewards/view/reward_list.dart
@@ -22,7 +22,7 @@ class RewardsList extends StatelessWidget {
           ),
         ),
         TextSpan(
-          text: "Rewards",
+          text: " Rewards",
           style: Theme.of(context).textTheme.headline3,
         ),
       ]),
