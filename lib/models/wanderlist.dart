808ccb4bc8bb417fc8e62434eb858476655fa53c
Alistair <72960743+ailrst@users.noreply.github.com>
2021-10-27 16:48:17 +1000

    default list icon (#78)

--- lib/models/wanderlist.dart
+++ lib/models/wanderlist.dart
@@ -4,12 +4,12 @@ import 'package:application/models/activity.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
+
 @immutable
 class Wanderlist extends Equatable {
   Wanderlist(this.id, this.name, this.creatorName, this.activities, this.icon);
 
   factory Wanderlist.fromJson(Map<String, dynamic> json) {
-    print(json);
     List<ActivityDetails>? activities = List<ActivityDetails>.from(
         json['activities'].map((activity) {
               return ActivityDetails.fromJson(activity);}
